f9c87ec0592e4fd8cebf7760b68d116949ff4f3c
aqdasmalik <aqdasmalik8@gmail.com>
2022-07-02 13:33:12 +0100

    Initial Commit

---
+++ lib/craq_validation.ex
@@ -0,0 +1,18 @@
+defmodule CraqValidation do
+  @moduledoc """
+  Documentation for `CraqValidation`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CraqValidation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/craq_validation_test.exs
@@ -0,0 +1,8 @@
+defmodule CraqValidationTest do
+  use ExUnit.Case
+  doctest CraqValidation
+
+  test "greets the world" do
+    assert CraqValidation.hello() == :world
+  end
+end
